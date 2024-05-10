2f1391e14ade0540c7d1d90283db28f9b6e85223
Sagar <dev.sagartmg@gmail.com>
2024-05-10 17:09:11 +0545

    theme change & make routes

--- react-fundamentals/src/App.jsx
+++ react-fundamentals/src/App.jsx
@@ -1,7 +1,7 @@
 import LatestProducts from "./LatestProducts";
 import TrendingProducts from "./TrendingProducts";
 import Todos from "./Todos";
-import Counter from "./Counter"
+import Counter from "./Counter";
 import CounterNew from "./CounterNew";
 import ProductsApi from "./ProductsApi";
 import Login from "./Login";
@@ -9,15 +9,70 @@ import ProductSearch from "./ProductSearch";
 import GoogleTabs from "./GoogleTabs";
 import ChangeTheme from "./ChangeTheme";
 import Header from "./Header";
+import { useState } from "react";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  Outlet,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 let title = "conditional rendering";
 
-
 export default function App() {
+  const [theme, setTheme] = useState("light");
+
+  const router = createBrowserRouter([
+    {
+      path: "",
+      element: (
+        <>
+          <Header theme={theme} setTheme={setTheme} />
+          <Outlet />
+        </>
+      ),
+      children: [
+        {
+          path: "/",
+          element:<Home/>
+        },
+        {
+          path: "about",
+          element: <About/>
+        },
+        {
+          path: "products",
+          children: [
+            {
+              path: "",
+              element: (
+                <div>
+                  <h1>Products</h1>
+                </div>
+              ),
+            },
+            {
+              path: "watch",
+              element: (
+                <div>
+                  <h1>Watch Page</h1>
+                </div>
+              ),
+            },
+          ],
+        },
+      ],
+    },
+  ]);
+
   return (
-    <div>
-      <Header/>
-      <h1 style={{textAlign:"center"}}>{title}</h1>
+    <div className={theme == "dark" ? "dark" : ""}>
+      <RouterProvider router={router} />
+
       {/* <TrendingProducts /> */}
       {/* <LatestProducts /> */}
       {/* <Counter/> */}
@@ -25,11 +80,10 @@ export default function App() {
       {/* <Todos /> */}
       {/* <ProductsApi/> */}
       {/* <ProductSearch/> */}
-      <GoogleTabs/>
+      {/* <GoogleTabs /> */}
       {/* <ChangeTheme/> */}
       {/* <Login/> */}
       {/* theme change: light /dark */}
-
     </div>
   );
 }
--- react-fundamentals/src/Header.jsx
+++ react-fundamentals/src/Header.jsx
@@ -1,20 +1,35 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
-export default function Header() {
-  const [theme, setTheme] = useState("light");
+export default function Header({ theme, setTheme }) {
+
   const [language, setLanguage] = useState("english");
+  // const [theme, setTheme] = useState("light");
 
-  useEffect(() => {
-    console.log("header-useEffect - componenent did update ");
-    document.getElementById("body").classList.toggle("dark");
-  },[theme]);
+  
+
+
+
+  /*
+    
+    useEffect(() => {
+      console.log("header-useEffect - componenent did update ");
+      // document.getElementById("body").classList.toggle("dark");
+
+    },[theme]); 
+    
+    */
 
   return (
     <div className={`header`}>
       <div>
-        <a href="">home</a>
-        <a href="">contact</a>
+        {/* <a href="/">home</a>
+        <a href="/about">about</a> */}
+        <Link to="/">home</Link>&nbsp;
+        <Link to="/about">about</Link>&nbsp;
+        <Link to="/products">products</Link>&nbsp;
+        <Link to="/products/watch">watch</Link>&nbsp;
       </div>
       <div>
         <button
@@ -27,6 +42,7 @@ export default function Header() {
         currentlanguage: {language}&nbsp; currentTheme: {theme}&nbsp;
         <button
           onClick={() => {
+            /* setTheme function  of App */
             setTheme(theme == "light" ? "dark" : "light");
           }}
         >
