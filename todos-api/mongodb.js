ab477dbc8c28d83a4f712c51f3716063ba120fd4
Sagar <dev.sagartmg@gmail.com>
2024-05-17 16:02:35 +0545

    link express with todos database

--- todos-api/mongodb.js
+++ todos-api/mongodb.js
@@ -23,10 +23,9 @@ db.students.insertMany([
   },
 ])
 
-
 db.todos.insertMany([
-    {title:"css",status:false},
-    {title:"js",status:false},
+  { title: "css", status: false },
+  { title: "js", status: false },
 ])
 
 db.todos.find()
@@ -34,7 +33,75 @@ db.todos.find()
 db.students.find()
 // db.students.find(<filter options>)
 
-db.students.find({name:"ram"})
+db.students.find({ name: "ram" })
 
 db.students.updateOne({})
 
+// use store
+db.sales.insertOne({
+  buyer: "Ram",
+  phone: 13123,
+  products: ["watch", "projector"],
+})
+
+db.sales.insertOne({
+  buyer: "Hari",
+  phone: 13123,
+  products: [
+    {
+      title: "Watch",
+      price: 1000,
+    },
+    {
+      title: "projector",
+      price: 500000,
+    },
+  ],
+})
+
+let sales = [
+  {
+    _id: ObjectId("6647220a52155a656d04d170"),
+    buyer: "Ram",
+    phone: 13123,
+    products: ["watch", "projector"],
+  },
+  {
+    _id: ObjectId("6647225352155a656d04d171"),
+    buyer: "Hari",
+    phone: 13123,
+    products: [
+      {
+        title: "Watch",
+        price: 1000,
+      },
+      {
+        title: "projector",
+        price: 500000,
+      },
+    ],
+  },
+]
+
+sales.map((sale) => {
+  return <li> {sale.buyer} </li>
+})
+
+let todos = [
+  { _id: ObjectId("6645d6872e77b9df371d46bc"), title: "html" },
+  {
+    _id: ObjectId("6645d6c82e77b9df371d46bd"),
+    title: "css",
+    status: false,
+  },
+  {
+    _id: ObjectId("6645d6c82e77b9df371d46be"),
+    title: "js",
+    status: false,
+  },
+  {
+    _id: ObjectId("6647234852155a656d04d172"),
+    name: "express",
+    status: "false",
+  },
+]
--- todos-api/todos-api.js
+++ todos-api/todos-api.js
@@ -1,23 +1,38 @@
 const express = require("express")
 const app = express()
+
+
 const mongoose = require("mongoose")
-
-mongoose.connect("mongodb://127.0.0.1:27017/todos").then(() => console.log("DB Connected!"))
-
 const Schema = mongoose.Schema
-const ObjectId = Schema.ObjectId
 
-const TodoSchema = new Schema({
+const TodosSchema = new Schema({
   title: String,
   status: Boolean,
 })
 
-const Todo = mongoose.model("Todo", TodoSchema)
+const Todo = mongoose.model("Todo", TodosSchema)
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/todos")
+  .then(() => console.log("DB Connected!"))
 
 app.get("/api/todos", async (req, res) => {
+  // db.todos.find()
   let todos = await Todo.find()
+  console.log(todos)
   res.send(todos)
-  //   res.send(["html", "mongodb"])
+  // res.send(["html", "mongodb"])
+})
+
+app.post("/api/todos", async (req, res) => {
+  // db.todos.insertOne({title:})
+  let todo = await Todo.create({title:"html"})
+  res.send(todo)
+})
+
+app.put("/api/todos/:id", async (req, res) => {
+  let todo = await Todo.findByIdAndUpdate("66472d84d9509b539993a095",{title:"htasdfasdfml"})
+  res.send(todo)
 })
 
 app.listen(8000, () => {
